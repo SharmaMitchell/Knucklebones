5d628a00bda7f3280f707fad6002b2710ee31c31
Mitchell Sharma <sharmamitch@gmail.com>
2023-09-23 21:46:09 -0500

    Initial Commit

---
+++ Knucklebones_Prototype/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Knucklebones_Prototype
+//
+//  Created by Mitchell Sharma on 9/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Knucklebones_Prototype/Knucklebones_PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Knucklebones_PrototypeApp.swift
+//  Knucklebones_Prototype
+//
+//  Created by Mitchell Sharma on 9/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Knucklebones_PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
